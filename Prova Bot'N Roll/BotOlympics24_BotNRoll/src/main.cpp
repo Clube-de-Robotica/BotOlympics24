9e583d490e5de586fcd3b3785312c990f1c3aa70
tiagoluis19 <112717495+tiagoluis19@users.noreply.github.com>
2024-04-11 14:50:52 +0100

    Update main.cpp

--- Prova Bot'N Roll/BotOlympics24_BotNRoll/src/main.cpp
+++ Prova Bot'N Roll/BotOlympics24_BotNRoll/src/main.cpp
@@ -60,134 +60,8 @@ BnrOneA one;
 /* USER CODE BEGIN FUNCTION PROTOTYPES */
 
 // place your functions here
-int checkWhite(int indice)
-{
-  int limiar = 100;
-  if (one.readAdc(indice) < limiar)
-    return 1;
-  else
-    return 0;
-}
 
-int checkBlack(int indice)
-{
-  int limiar = 500;
-  if (one.readAdc(indice) > limiar)
-    return 1;
-  else
-    return 0;
-}
 
-int allWhite()
-{
-  int limiar = 100;
-  int counter = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    if (checkWhite(i))
-      counter++;
-  }
-  if (counter >= 7)
-  {
-    return 1;
-  }
-  else
-    return 0;
-}
-
-bool allBlack()
-{
-  int limiar = 100;
-  int counter = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    if (checkBlack(i))
-      counter++;
-  }
-  if (counter >= 7)
-  {
-    return true;
-  }
-  else
-    return false;
-}
-
-int mindex(int leituras[])
-{
-
-  int min = leituras[0];
-  int mindex = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    if (leituras[i] < min)
-    {
-      min = leituras[i];
-      mindex = i;
-    }
-  }
-
-  return mindex;
-};
-int readline()
-{
-  int leituras[8];
-  int limiar = 100;
-  // int maxval=0;
-  // int minval=0;
-
-  // if (allWhite() || allBlack())
-  // {
-  //   return -1;                  Joca isto e 100% do tempo seu burro da sempre -1
-  // }
-
-  for (int i = 0; i < 8; i++)
-  {
-    leituras[i] = one.readAdc(i);
-  }
-
-  if (leituras[3] <= limiar && leituras[4] <= limiar)
-  {
-    return 0;
-  }
-
-  if (mindex(leituras) < 3)
-  {
-
-    return (map((leituras[0] * 10 + leituras[1] * 8 + leituras[2] * 3 + leituras[3] * 1) / (10 + 8 + 3 + 1), 0, 1023, 0, 100));
-  }
-
-  else if (mindex(leituras) > 4)
-  {
-    return (map((leituras[7] * (-10) + leituras[6] * (-8) + leituras[5] * (-3) + leituras[4] * (-1)) / (10 + 8 + 3 + 1), -1023, 0, -100, 0));
-  }
-  else
-    return 0;
-};
-
-int readwall()
-{
-
-  int limiar = 200; // distancia lidar direita
-  int dist = Lidar.getLidarRightDistance();
-
-  if (dist == limiar)
-  {
-    return 0;
-  }
-
-  if (dist > limiar)
-  {
-
-    return (map(dist, 0, 2600, 0, 100));
-  }
-
-  else if (dist < limiar)
-  {
-    return (map(dist, 0, 2600, -100, 0));
-  }
-  else
-    return 0;
-};
 
 //  The setup function runs once when you press reset or power the board
 void setup()
@@ -200,7 +74,7 @@ void setup()
   one.minBat(3);             // set batery treshold
   Lidar.begin();
   /* USER CODE BEGIN Initializations */
-  one.magnet(1);
+ 
   // place your initializations here
 
   /* USER CODE END Initializations */
@@ -252,89 +126,8 @@ void setup()
     delay(2000); // wait 2sec
   }
 }
-int estado = 0;
+
 //  The loop function runs over and over again forever
 void loop()
-{Serial.print("aa");
-  int speed = 30;
-  int speedL = 0, speedR = 0;
-  int curvespeed = 5; // limita a velocidade da roda interior
-
-  int gain = 80; // limita o ganho
-
-  if (one.readAdc(0) < 100 &&
-      one.readAdc(1) < 100 &&
-      one.readAdc(2) < 100 &&
-      one.readAdc(3) < 100 &&
-      one.readAdc(4) < 100 &&
-      one.readAdc(5) < 100 &&
-      one.readAdc(6) < 100 &&
-      one.readAdc(7) < 100)
-  {
-    one.magnet(0);
-    estado = 1;
-    one.move(-30, -30);
-    delay(1500);
-    one.move(-30, 30);
-    delay(500);
-  }
-  switch (estado)
-  {
-  case 0:
-  {
-    int line = readline();Serial.println(line);
-    if (line < 0)
-    {
-      speedR = (speed - (line * line)) / gain;
-
-      speedL = speed + (speed - speedL);
-    }
-    else
-    {
-      speedL = (speed - (line * line)) / gain;
-
-      speedR = speed + (speed - speedR);
-    }
-
-    if (speedL < -1)
-      speedL = -1;
-    if (speedR < -1)
-      speedR = -1;
-    if (speedL > speed + curvespeed)
-      speedL = speed + curvespeed;
-    if (speedR > speed + curvespeed)
-      speedR = speed + curvespeed;
-
-    one.move(speedL, speedR);
-    break;
-  }
-  case 1:
-  {
-    int wall = readwall();
-    if (wall < 0)
-    {
-      speedR = (speed - (wall * wall)) / gain;
-
-      speedL = speed + (speed - speedL);
-    }
-    else
-    {
-      speedL = (speed - (wall * wall)) / gain;
-
-      speedR = speed + (speed - speedR);
-    }
-
-    if (speedL < -1)
-      speedL = -1;
-    if (speedR < -1)
-      speedR = -1;
-    if (speedL > speed + curvespeed)
-      speedL = speed + curvespeed;
-    if (speedR > speed + curvespeed)
-      speedR = speed + curvespeed;
-
-    one.move(speedR, speedL);
-    break;
-  }
-  }
+    
 }
